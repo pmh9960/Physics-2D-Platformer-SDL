81fe0f9c52e35d5fc22cef54400b81b41eb0759f
Park Minho <pmh9960@korea.ac.kr>
2020-08-02 02:26:33 +0900

    Render Window
    1. Make Render window header file
     - RenderWindow class
    2. Make Render window cpp file
     - Explain variable and function in RenderWindow class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
+
+#include <RenderWindow.hpp>
 
 int main(int argc, char *args[])
 {
-    std::cout << "hello" << std::endl;
-    std::cin.get();
+    if (SDL_Init(SDL_INIT_VIDEO) > 0)
+        std::cout << "HEY... SDL INIT HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+    if (!(IMG_Init(IMG_INIT_PNG)))
+        std::cout << "HEY... IMG INIT HAS FAILED. IMG_ERROR: " << SDL_GetError() << std::endl;
+
+    RenderWindow window("GAME v1.0", 1280, 720);
+
+    bool gameRunning = true;
+    SDL_Event event;
+
+    while (gameRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                gameRunning = false;
+        }
+    }
+
+    window.cleanUp();
+    SDL_Quit();
+
     return 0;
 }
